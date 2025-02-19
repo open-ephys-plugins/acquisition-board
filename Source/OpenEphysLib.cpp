a7b0033a110b3c99b129e6d9bc1469e8e0a764bb
Anjal Doshi <anjal.doshi@alleninstitute.org>
2025-02-18 16:33:30 -0800

    Bump plugin version to 0.1.1

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -41,7 +41,7 @@ extern "C" EXPORT void getLibInfo(Plugin::LibraryInfo* info)
 {
 	info->apiVersion = PLUGIN_API_VER;
 	info->name = "Acquisition Board";
-	info->libVersion = "0.1.0";
+	info->libVersion = "0.1.1";
 	info->numPlugins = NUM_PLUGINS;
 }
 
